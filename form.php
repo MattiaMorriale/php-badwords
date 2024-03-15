74655e2964bed8e034129dd54cbb6fab532a7196
mattia_morriale <mattia.morriale1@gmail.com>
2024-03-15 15:42:01 +0100

    first commit

---
+++ form.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.s0">
+    <title>PHP-badwords</title>
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body data-bs-theme='dark'>
+    <div class="container-fluid p-2 ">
+
+        <h1 class='my-4'>Complete our form</h1>
+        <form action="">
+            <div class="mb-3">
+                <label class="form-label">Phrase</label>
+                <input type="text" class="form-control" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Bad Word</label>
+                <input type="text" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+
+    <!-- SCRIPT-BOOTSTRAP -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
